3678a7ba33a7ed0694abcaf45dbc8baf699118e4
fiuzarthur <fiuzarthur@gmail.com>
2023-02-14 18:00:46 -0300

    Create Project

---
+++ Source/ArthurFiuzaCProject/ArthurFiuzaCProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArthurFiuzaCProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArthurFiuzaCProject, "ArthurFiuzaCProject" );
